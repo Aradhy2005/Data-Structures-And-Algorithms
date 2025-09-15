2db9968984edf12750b6d84541415ec06dd23085
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2025-09-15 05:41:05 +0530

    Time: 17 ms (72.66%), Space: 43.7 MB (90.74%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,49 +1,47 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i++)
         {
             if(i>0 && nums[i]==nums[i-1])continue;
 
             for(int j=i+1;j<nums.length;j++)
             {
-                if(j!=(i+1) && nums[j]==nums[j-1])continue;
-
+                if(j>i+1 && nums[j]==nums[j-1])continue;
                 int k=j+1;
                 int l=nums.length-1;
 
-                while(k<l){
-
-                long sum=nums[i];
-                sum+=nums[j];
-                sum+=nums[k];
-                sum+=nums[l];
-
-                if(sum==target)
+                while(k<l)
                 {
-                   List<Integer> arr = new ArrayList<>();
-                   arr.add(nums[i]);
-                   arr.add(nums[j]);
-                   arr.add(nums[k]);
-                   arr.add(nums[l]);
+                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
 
-                   res.add(arr);
-                   k++;
-                   l--;
-                   while(k<l && nums[k-1]==nums[k])k++;
+                    if(sum==target)
+                    {
+                       List<Integer> temp = new ArrayList<>();
+                       temp.add(nums[i]);
+                       temp.add(nums[j]);
+                       temp.add(nums[k]);
+                       temp.add(nums[l]);
+                       res.add(temp);
 
-                   while(k<l && nums[l]==nums[l+1])l--;
-                }
-                else if(sum<target)k++;
-                else l--;
+                       k++;
+                       l--;
+
+                       while(k<l && nums[k]==nums[k-1])k++;
+                       while(k<l && nums[l]==nums[l+1])l--;
+
+                    }
+
+                    else if(sum<target)k++;
+
+                    else l--;
 
                 }
             }
         }
 
         return res;
-        
+
     }
 }
