f3a9792bd780b50ed4c422b3f7032cbb29ebf266
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2025-09-24 06:30:58 +0530

    Time: 1 ms (84.55%), Space: 40.9 MB (54.31%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        int low=0;
+        int high=x/2;
+        if(x<2)return x;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            long sq=(long)mid*mid;
+
+            if(sq==x)return mid;
+
+            else if(sq>x)high=mid-1;
+
+            else low=mid+1;
+        }
+
+        return high;
+        
+    }
+}
