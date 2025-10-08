3dd7964ee907c2e9d95e28e5914526becfb6201d
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2025-10-08 11:37:57 +0530

    Time: 2 ms (99.74%), Space: 51 MB (9.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,51 +1,52 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+      int maj1=0,maj2=0,cnt1=0,cnt2=0;
+      for(int i=0;i<nums.length;i++)
+      {
 
-        int cnt1=0;
-        int cnt2=0;
-        int el1= Integer.MIN_VALUE;
-        int el2= Integer.MIN_VALUE;
+         if(maj1==nums[i])
+         cnt1++;
 
-        for(int i=0;i<nums.length;i++)
-        {
-            if(cnt1==0 && el2!=nums[i])
-            {
-                cnt1=1;
-                el1=nums[i];
-            }
+         else if(maj2==nums[i])
+         cnt2++;
 
-            else if(cnt2==0 && el1!=nums[i])
-            {
-                cnt2=1;
-                el2=nums[i];
-            }
+         else if(cnt1==0)
+         {
+            maj1=nums[i];
+            cnt1=1;
+         }
 
-            else if(nums[i]==el1)cnt1++;
+         else if(cnt2==0)
+         {
+            maj2=nums[i];
+            cnt2=1;
+         }
 
-            else if(nums[i]==el2)cnt2++;
+         else{
+            cnt1--;
+            cnt2--;
+         }
 
-            else 
-            {
-                cnt1--;
-                cnt2--;
-            }
-        }
+      } 
 
-        cnt1=0;
-        cnt2=0;
+      List<Integer> ans = new ArrayList<>();
+      cnt1=0;
+      cnt2=0;
 
-        ArrayList<Integer> res = new ArrayList<>();
+      for(int i=0;i<nums.length;i++)
+      {
+           if(nums[i]==maj1)cnt1++;
 
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==el1)cnt1++;
+           else if(nums[i]==maj2)cnt2++;
 
-            else if(nums[i]==el2)cnt2++;
-        }
 
-        if(cnt1>(nums.length)/3)res.add(el1);
-        if(cnt2>(nums.length)/3)res.add(el2);
+      }
 
-        return res;
+      if(cnt1>(nums.length/3))ans.add(maj1);
+
+      if(cnt2>(nums.length/3))ans.add(maj2);
+
+      return ans;
+        
     }
 }
