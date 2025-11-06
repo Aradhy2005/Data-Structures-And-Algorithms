26de4f4a6eb59bf1ee895fc63ce3cee1e0d390e1
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2025-11-06 09:52:54 +0530

    Time: 15 ms (46.75%), Space: 93 MB (5.04%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> st= new HashSet<>();
+
+        for(int i:nums)
+        {
+            if(st.contains(i))
+            return true;
+
+            else
+            st.add(i);
+
+        }
+
+        return false;
+        
+    }
+}
