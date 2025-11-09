1080e848d3322c903bab7074cb4bef43038df949
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2025-11-09 10:23:17 +0530

    Time: 4 ms (38.39%), Space: 45.4 MB (6.4%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,38 +1,37 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        int [] ans=new int[nums1.length];
+        int ans[]=new int[nums1.length];
 
-        int k=0;
+        Stack<Integer> st=new Stack<>();
+
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+
+        for(int i=nums2.length-1;i>=0;i--)
+        {
+
+            while((!st.isEmpty()) && st.peek()<=nums2[i])
+            {
+                st.pop();
+            }
+
+            if(st.isEmpty())
+            {
+                mpp.put(nums2[i],-1);
+            }
+
+            else{
+                mpp.put(nums2[i],st.peek());
+            }
+
+            st.push(nums2[i]);
+            
+        }
+
 
         for(int i=0;i<nums1.length;i++)
         {
-            int n=nums1[i];
-
-            for(int j=0;j<nums2.length;j++)
-            {
-                int flag=0;
-                if(nums1[i]==nums2[j])
-                {
-                    int temp=j+1;
-
-                    while(temp<nums2.length)
-                    {
-                        if(nums2[temp]>nums1[i])
-                        {
-                            ans[k++]=nums2[temp];
-                            flag=1;
-                            break;
-
-                        }
-
-                        temp++;
-
-                    }
-
-                    if(flag==0)ans[k++]=-1;
-                }
-            }
+            ans[i]=mpp.get(nums1[i]); 
         }
 
         return ans;
