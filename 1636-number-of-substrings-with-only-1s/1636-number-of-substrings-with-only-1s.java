80b331e8c67cb095ad7c498964a79e76d04b1e4d
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2025-11-17 05:33:39 +0530

    Time: 5 ms (56.95%), Space: 46.4 MB (9.63%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -1,28 +1,21 @@
 class Solution {
     public int numSub(String s) {
-
-        long cnt=0;
-        long m=1000000007;
         long res=0;
+        long cnt=0;
+        int m=1000000007;
+
         for(int i=0;i<s.length();i++)
         {
-            char c=s.charAt(i);
-            if(c=='1')
+
+            if(s.charAt(i)=='1')
             {
                 cnt++;
-               
+                res=(res+cnt)%m;
             }
 
-            else if(c=='0')
-            {
-                 res=res+(cnt*(cnt+1)/2)%m;
-                cnt=0;
-            }
-
-
+            else
+            cnt=0;
         }
-
-        res=res+(cnt*(cnt+1)/2)%m;
 
         return (int)res;
         
