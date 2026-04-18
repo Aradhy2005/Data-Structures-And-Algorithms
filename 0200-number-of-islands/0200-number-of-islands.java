4ef0525a96e06e679dd2ec969ce0ba1a30261957
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2026-04-18 10:59:15 +0530

    Time: 2 ms (99.89%), Space: 52 MB (74.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,18 +3,16 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
 
-        boolean vis[][] = new boolean[n][m];
-
         int cnt=0;
 
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1' && !vis[i][j])
+                if(grid[i][j]=='1')
                 {
                     cnt++;
-                    dfs(i,j,grid,vis);
+                    dfs(i,j,grid);
                 }
             }
         }
@@ -22,19 +20,19 @@ class Solution {
         return cnt;
     }
 
-    public void dfs(int i,int j,char[][] grid,boolean[][] vis)
+    public void dfs(int i,int j,char[][] grid)
     {
         int n=grid.length;
         int m=grid[0].length;
 
-        if(i<0 ||j<0 ||i>=n ||j>=m ||grid[i][j]=='0'|| vis[i][j])
+        if(i<0 ||j<0 ||i>=n ||j>=m ||grid[i][j]=='0')
         return;
 
-        vis[i][j]=true;
+        grid[i][j]='0';
 
-        dfs(i+1,j,grid,vis);
-        dfs(i-1,j,grid,vis);
-        dfs(i,j+1,grid,vis);
-        dfs(i,j-1,grid,vis);
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i,j-1,grid);
     }
 }
