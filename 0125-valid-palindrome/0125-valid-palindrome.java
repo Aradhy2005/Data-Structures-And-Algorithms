2fe0313662445104e8f1150e9c65e22da07fa5a5
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2026-07-04 08:12:06 +0530

    Time: 3 ms (60.67%), Space: 44.2 MB (84.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,35 +3,21 @@ class Solution {
 
         s=s.toLowerCase();
 
-        StringBuilder res= new StringBuilder();
-
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            if(Character.isLetterOrDigit(ch))
-            {
-                res.append(ch);
-            }
-        }
-
         int i=0;
-        int j=res.length()-1;
+        int j=s.length()-1;
 
         while(i<j)
         {
-            if(res.charAt(i)!=res.charAt(j))
-            {
-                return false;
-                
-            }
+            while(i<j && !(Character.isLetterOrDigit(s.charAt(i))))i++;
+            while(i<j && !(Character.isLetterOrDigit(s.charAt(j))))j--;
+
+            if(s.charAt(i)!=s.charAt(j))return false;
+
             i++;
             j--;
         }
 
         return true;
-
-
-
         
     }
 }
