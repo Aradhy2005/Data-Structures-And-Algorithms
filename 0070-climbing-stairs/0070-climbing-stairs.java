514aca733a798e5d0ef3c8c818cd019ce1034c71
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2026-07-20 07:27:03 +0530

    Time: 0 ms (100%), Space: 42 MB (54.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,8 +7,8 @@ class Solution {
 
     public int helper(int n,int[] dp)
     {
-        if(n<=1)return 1;
-
+        if(n<0)return 0;
+        if(n==0)return 1;
         if(dp[n]!=-1)return dp[n];
 
         return dp[n]=helper(n-1,dp)+helper(n-2,dp);
