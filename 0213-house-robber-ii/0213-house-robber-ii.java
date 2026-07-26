d3ab2f7af3fdcd566fb2fe35b2172e0103b1d36e
Aradhy Bajpai <bajpaiaradhy@gmail.com>
2026-07-26 09:38:02 +0530

    Time: 1 ms (4.23%), Space: 42.9 MB (42.16%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -9,30 +9,32 @@ class Solution {
         for(int i=0;i<nums.length-1;i++)temp1.add(nums[i]);
         for(int i=1;i<nums.length;i++)temp2.add(nums[i]);
 
-        int[] dp1 = new int[temp1.size()];
-        int[] dp2 = new int[temp2.size()];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
-
-        int ans1 = helper(temp1,temp1.size()-1,dp1);
-        int ans2 = helper(temp2,temp2.size()-1,dp2);
+        int ans1 = helper(temp1);
+        int ans2 = helper(temp2);
 
         return Math.max(ans1,ans2);
 
         
     }
 
-    public int helper(List<Integer>temp , int i,int[] dp)
+    public int helper(List<Integer>temp)
     {
-        if(i==0)return temp.get(0);
-        if(i<0)return 0;
+        int[] dp = new int[temp.size()];;
+        dp[0]=temp.get(0);
 
-        if(dp[i]!=-1)return dp[i];
 
-        int pick=0;
-        pick = temp.get(i)+helper(temp,i-2,dp);
-        int nonPick = 0+helper(temp,i-1,dp);
+        for(int i=1;i<temp.size();i++)
+        {
+            int pick = temp.get(i);
 
-        return dp[i] = Math.max(pick,nonPick);
+            if(i>1)
+            pick+=dp[i-2];;
+
+            int nonPick = dp[i-1];
+
+            dp[i]=Math.max(pick,nonPick);
+        }
+
+        return dp[temp.size()-1];
     }
 }
